b988416092c809a12a6ffa85152d8df4c7e8d309
TanCan1998 <tancan1998@qq.com>
2018-09-26 23:11:57 +0800

    第四次提交
    第四次提交

--- index.php
+++ index.php
@@ -17,35 +17,24 @@
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>1</td>
-        <td><a href="show.php?id=1">第一篇博客</a></td>
-        <td>2012-10-10  21:07:57</td> 
-        <td>
-          <a href="edit.php?id=1">改</a>
-          <a href="delete.php?id=1">删</a>
-        </td> 
-      </tr> 
-      
-      <tr>
-        <td>2</td>
-        <td><a href="show.php?id=2">第二篇博客</a></td>
-        <td>2012-10-11  16:26:21</td> 
-        <td>
-          <a href="edit.php?id=2">改</a>
-          <a href="delete.php?id=2">删</a>
-        </td> 
-      </tr> 
-      
-      <tr>
-        <td>3</td>
-        <td><a href="show.php?id=3">第三篇博客</a></td>
-        <td>2012-12-12  19:38:44</td> 
-        <td>
-          <a href="edit.php?id=3">改</a>
-          <a href="delete.php?id=3">删</a>
-        </td> 
-      </tr> 
+      <?php
+        $posts = Array(
+            Array('id'=>'1','title'=>'第1篇帖子','created_at'=>'2012-1-1  21:01:57'),
+            Array('id'=>'2','title'=>'第2篇帖子','created_at'=>'2012-1-1  21:01:57'),
+            Array('id'=>'3','title'=>'第3篇帖子','created_at'=>'2012-1-1  21:01:57'),
+        );
+        foreach ($posts as $post) {
+      ?>
+          <tr>
+            <td><?php echo $post['id'] ?></td>
+            <td><a href="show.php?id=<?php echo $post['id'] ?>"><?php echo $post['title'] ?></a></td>
+            <td>2012-10-11  16:26:21</td> 
+            <td>
+              <a href="edit.php?id=<?php echo $post['id'] ?>">改</a>
+              <a href="delete.php?id=<?php echo $post['id'] ?>">删</a>
+            </td> 
+          </tr> 
+        <?php } ?>
       
     </tbody>
   </table>
